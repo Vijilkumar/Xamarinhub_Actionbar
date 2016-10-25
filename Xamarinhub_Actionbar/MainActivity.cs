8ad2451afba52efd02c7049ac6a7809ef341398c
Vijilkumar V <vijilkmrv2@hotmail.com>
2016-10-25 23:02:40 +0530

    Slide bar.. unfinish

--- Xamarinhub_Actionbar/MainActivity.cs
+++ Xamarinhub_Actionbar/MainActivity.cs
@@ -1,30 +1,170 @@
 ﻿using System;
 using Android.App;
-using Android.Content;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Views.Animations;
+using System.Linq;
+using Android.Graphics;
+using Xamarinhub_Actionbar.Classes;
 
 namespace Xamarinhub_Actionbar
 {
     [Activity(Label = "Xamarinhub_Actionbar", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
 
+        GestureDetector gestureDetector;
+        GestureListener gestureListener;
+
+        ListView menuListView;
+        MenuListAdapterClass objAdapterMenu;
+        ImageView menuIconImageView;
+        int intDisplayWidth;
+        bool isSingleTapFired = false;
+        TextView txtActionBarText;
+        TextView txtPageName;
+        TextView txtDescription;
+        ImageView btnDescExpander;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
+            Window.RequestFeature(WindowFeatures.NoTitle);
             SetContentView(Resource.Layout.Main);
+            FnInitialization();
+            TapEvent();
+            FnBindMenu(); //find definition in below steps
+        }
+        void TapEvent()
+        {
+            //title bar menu icon
+            menuIconImageView.Click += delegate (object sender, EventArgs e)
+            {
+                if (!isSingleTapFired)
+                {
+                    FnToggleMenu();  //find definition in below steps
+                    isSingleTapFired = false;
+                }
+            };
+            //bottom expandable description window
+            btnDescExpander.Click += delegate (object sender, EventArgs e)
+            {
+                FnDescriptionWindowToggle();
+            };
+        }
+        void FnInitialization()
+        {
+            //gesture initialization
+            gestureListener = new GestureListener();
+            gestureListener.LeftEvent += GestureLeft; //find definition in below steps
+            gestureListener.RightEvent += GestureRight;
+            gestureListener.SingleTapEvent += SingleTap;
+            gestureDetector = new GestureDetector(this, gestureListener);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            menuListView = FindViewById<ListView>(Resource.Id.menuListView);
+            menuIconImageView = FindViewById<ImageView>(Resource.Id.menuIconImgView);
+            txtActionBarText = FindViewById<TextView>(Resource.Id.txtActionBarText);
+            txtPageName = FindViewById<TextView>(Resource.Id.txtPage);
+            txtDescription = FindViewById<TextView>(Resource.Id.txtDescription);
+            btnDescExpander = FindViewById<ImageView>(Resource.Id.btnImgExpander);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            //changed sliding menu width to 3/4 of screen width 
+            Display display = this.WindowManager.DefaultDisplay;
+            var point = new Point();
+            display.GetSize(point);
+            intDisplayWidth = point.X;
+            intDisplayWidth = intDisplayWidth - (intDisplayWidth / 3);
+            using (var layoutParams = (RelativeLayout.LayoutParams)menuListView.LayoutParameters)
+            {
+                layoutParams.Width = intDisplayWidth;
+                layoutParams.Height = ViewGroup.LayoutParams.MatchParent;
+                menuListView.LayoutParameters = layoutParams;
+            }
+        }
+        void FnBindMenu()
+        {
+            string[] strMnuText = { GetString(Resource.String.Home), GetString(Resource.String.AboutUs), GetString(Resource.String.Products), GetString(Resource.String.Events), GetString(Resource.String.Serivce), GetString(Resource.String.Clients), GetString(Resource.String.Help), GetString(Resource.String.Solution), GetString(Resource.String.ContactUs) };
+            int[] strMnuUrl = { Resource.Drawable.Icon, Resource.Drawable.Icon, Resource.Drawable.Icon, Resource.Drawable.Icon, Resource.Drawable.Icon, Resource.Drawable.Icon, Resource.Drawable.Icon, Resource.Drawable.Icon, Resource.Drawable.Icon };
+            if (objAdapterMenu != null)
+            {
+                objAdapterMenu.actionMenuSelected -= FnMenuSelected;
+                objAdapterMenu = null;
+            }
+            objAdapterMenu = new MenuListAdapterClass(this, strMnuText, strMnuUrl);
+            objAdapterMenu.actionMenuSelected += FnMenuSelected;
+            menuListView.Adapter = objAdapterMenu;
+        }
+        void FnMenuSelected(string strMenuText)
+        {
+            txtActionBarText.Text = strMenuText;
+            txtPageName.Text = strMenuText;
+            //selected action goes here
+        }
+        void GestureLeft()
+        {
+            if (!menuListView.IsShown)
+                FnToggleMenu();
+            isSingleTapFired = false;
+        }
+        void GestureRight()
+        {
+            if (menuListView.IsShown)
+                FnToggleMenu();
+            isSingleTapFired = false;
+        }
+        void SingleTap()
+        {
+            if (menuListView.IsShown)
+            {
+                FnToggleMenu();
+                isSingleTapFired = true;
+            }
+            else
+            {
+                isSingleTapFired = false;
+            }
+        }
+        public override bool DispatchTouchEvent(MotionEvent ev)
+        {
+            gestureDetector.OnTouchEvent(ev);
+            return base.DispatchTouchEvent(ev);
+        }
+        void FnToggleMenu()
+        {
+            Console.WriteLine(menuListView.IsShown);
+            if (menuListView.IsShown)
+            {
+                menuListView.Animation = new TranslateAnimation(0f, -menuListView.MeasuredWidth, 0f, 0f);
+                menuListView.Animation.Duration = 300;
+                menuListView.Visibility = ViewStates.Gone;
+            }
+            else
+            {
+                menuListView.Visibility = ViewStates.Visible;
+                menuListView.RequestFocus();
+                menuListView.Animation = new TranslateAnimation(-menuListView.MeasuredWidth, 0f, 0f, 0f);//starting edge of layout 
+                menuListView.Animation.Duration = 300;
+            }
+        }
+
+        //bottom desription window sliding 
+        void FnDescriptionWindowToggle()
+        {
+            if (txtDescription.IsShown)
+            {
+                txtDescription.Visibility = ViewStates.Gone;
+                txtDescription.Animation = new TranslateAnimation(0f, 0f, 0f, txtDescription.MeasuredHeight);
+                txtDescription.Animation.Duration = 300;
+                btnDescExpander.SetImageResource(Resource.Drawable.Arrowup);
+            }
+            else
+            {
+                txtDescription.Visibility = ViewStates.Visible;
+                txtDescription.RequestFocus();
+                txtDescription.Animation = new TranslateAnimation(0f, 0f, txtDescription.MeasuredHeight, 0f);
+                txtDescription.Animation.Duration = 300;
+                btnDescExpander.SetImageResource(Resource.Drawable.Downarrow);
+            }
         }
     }
 }
--- Xamarinhub_Actionbar/Resources/Resource.Designer.cs
+++ Xamarinhub_Actionbar/Resources/Resource.Designer.cs
@@ -45,7 +45,16 @@ namespace Xamarinhub_Actionbar
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int Arrowup = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Downarrow = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int sidebar_icon = 2130837507;
 			
 			static Drawable()
 			{
@@ -60,8 +69,32 @@ namespace Xamarinhub_Actionbar
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050005
+			public const int btnImgExpander = 2131034117;
+			
+			// aapt resource value: 0x7f050007
+			public const int ivMenuImg = 2131034119;
+			
+			// aapt resource value: 0x7f050001
+			public const int menuIconImgView = 2131034113;
+			
+			// aapt resource value: 0x7f050006
+			public const int menuListView = 2131034118;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int titleBarLinearLayout = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int txtActionBarText = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int txtDescription = 2131034116;
+			
+			// aapt resource value: 0x7f050008
+			public const int txtMnuText = 2131034120;
+			
+			// aapt resource value: 0x7f050003
+			public const int txtPage = 2131034115;
 			
 			static Id()
 			{
@@ -79,6 +112,9 @@ namespace Xamarinhub_Actionbar
 			// aapt resource value: 0x7f030000
 			public const int Main = 2130903040;
 			
+			// aapt resource value: 0x7f030001
+			public const int MenuCustomLayout = 2130903041;
+			
 			static Layout()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -92,11 +128,38 @@ namespace Xamarinhub_Actionbar
 		public partial class String
 		{
 			
+			// aapt resource value: 0x7f040003
+			public const int AboutUs = 2130968579;
+			
 			// aapt resource value: 0x7f040001
 			public const int ApplicationName = 2130968577;
 			
+			// aapt resource value: 0x7f040007
+			public const int Clients = 2130968583;
+			
+			// aapt resource value: 0x7f04000a
+			public const int ContactUs = 2130968586;
+			
+			// aapt resource value: 0x7f040005
+			public const int Events = 2130968581;
+			
 			// aapt resource value: 0x7f040000
 			public const int Hello = 2130968576;
+			
+			// aapt resource value: 0x7f040008
+			public const int Help = 2130968584;
+			
+			// aapt resource value: 0x7f040002
+			public const int Home = 2130968578;
+			
+			// aapt resource value: 0x7f040004
+			public const int Products = 2130968580;
+			
+			// aapt resource value: 0x7f040006
+			public const int Serivce = 2130968582;
+			
+			// aapt resource value: 0x7f040009
+			public const int Solution = 2130968585;
 			
 			static String()
 			{
